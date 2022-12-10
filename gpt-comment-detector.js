070e96bbed153019ff77796299c24ab1eeedeb49
Your Name <you@example.com>
2022-12-10 15:07:38 +0100

    minor improvements and comments

--- gpt-comment-detector.js
+++ gpt-comment-detector.js
@@ -3,15 +3,10 @@
 // @namespace    https://example.com/
 // @version      0.1
 // @description  Identify AI-generated comments on Hacker News
-// @author       You
+// @author       chryzsh
 // @match        https://news.ycombinator.com/*
 // @grant        none
 // @connect      huggingface.co
-// @grant		GM_getValue
-// @grant		GM_setValue
-// @grant		GM_addElement
-// @grant		GM_registerMenuCommand
-// @grant   GM_xmlHttpRequest
 // @grant         GM.xmlHttpRequest
 // @grant		unsafeWindow
 // @sandbox		JavaScript
@@ -24,7 +19,6 @@ const TOKEN_THRESHOLD = 200; //4 characters are about 50 tokens, which is when t
 
 // put all comments into a NodeList
 const comments = document.querySelectorAll('.comment');
-//console.log(comments);
 
 // regex to remove whitespaces
 const whitespaceRegex = /\s+/g;
@@ -32,94 +26,86 @@ const whitespaceRegex = /\s+/g;
 //regex to remove "reply" at the end of comments
 const replyRegex = /\breply\b\s*/g;
 
-// loop over each comment, running the AI detection
-comments.forEach((comment) => {
+// kick off the program
+main();
+
+function main() {
+  // loop over each comment
+  comments.forEach((comment) => {
     // add an onclick for each comment
-  comment.addEventListener('click', function() {
-
-  // trim commments for whitespaces and test length for each comment
-  testLength(comment);
+    comment.addEventListener('click', function () {
+      // trim commments for whitespaces and test length for each comment
+      testLength(comment);
     });
-});
+  });
+};
 
-
+// Trim and test the length of comments
 function testLength(comment) {
-  // trim whitespaces and shit from the end first - dont do this on the comment object itself, just a variable
   var text = comment.textContent;
-  text = comment.textContent.trim();
 
+  // trim whitespaces and shit from the end first - dont do this on the comment object itself, just a variable
+  text = comment.textContent.trim();
   text = comment.textContent.replace(whitespaceRegex, " ");
+
   // remove "reply" from the end
   text = comment.textContent.replace(replyRegex, "");
 
-    // test the length first
-    if (text.length < TOKEN_THRESHOLD) {
-      comment.style.border = '1px solid gray';
-      comment.innerHTML += `<div style="color: gray; font-weight: bold;">Insufficient data to assess if AI generated</div>`;
-    }
-    else {
-      //console.log("enough data to assess: " + textLength);
-      detectAI(comment.textContent, function(result) {
-        addComment(result, comment);
-  });
+  // test the length first so we don't run detection on too short comments
+  if (text.length < TOKEN_THRESHOLD) {
+    comment.style.border = '1px solid gray';
+    comment.innerHTML += `<div style="color: gray; font-weight: bold;">Insufficient data to assess if AI generated</div>`;
+  }
+  else {
+    // run AI detection and add comment based on the result
+    detectAI(comment.textContent, function (result) {
+      addComment(result, comment);
+    });
 
-    }
+  }
 }
 
+// Add comment
 function addComment(result, comment) {
+  var json = JSON.parse(result);
+  var fakeProbability = json.fake_probability;
+  var realProbability = json.real_probability;
+  var formattedNumberAI = (fakeProbability * 100).toFixed(3);
+  var formattedNumberHuman = (realProbability * 100).toFixed(3);
 
-    //comments.forEach((comment) => {
-      var json = JSON.parse(result);
-      var fakeProbability = json.fake_probability;
-      var realProbability = json.real_probability;
-      var formattedNumberAI = (fakeProbability*100).toFixed(3);
-      var formattedNumberHuman = (realProbability*100).toFixed(3);
-      //console.log("fake prob: " + formattedNumberAI); // expected Output for input string "test" = 0.9973894953727722
-      //console.log("real prob: " + formattedNumberHuman)
+  // most comments will not be AI (hopefully) so start with testing that
+  if (fakeProbability < PROBABLE_THRESHOLD) { //assume not AI if lower than 0.7 on the AI-meter
+    comment.style.border = '1px solid green';
+    comment.innerHTML += `<div style="color: green; font-weight: bold;">Definitely human - human probability ${formattedNumberHuman}</div>`;
+  }
+  // If the probability is above the threshold, label the comment as AI-generated
+  else if (fakeProbability > PROBABLE_THRESHOLD) { // possibly AI
+    if (fakeProbability > AI_THRESHOLD) { //definitely AI
 
-      // most comments will not be AI (hopefully) so start with testing that
-      if (fakeProbability < PROBABLE_THRESHOLD) { //assume not AI if lower than 0.7 on the AI-meter
-          comment.style.border = '1px solid green';
-          comment.innerHTML += `<div style="color: green; font-weight: bold;">Definitely human - human probability ${formattedNumberHuman}</div>`;
-      }
-
-      // If the probability is above the threshold, label the comment as AI-generated
-     else if (fakeProbability > PROBABLE_THRESHOLD){ // possibly AI
-       if (fakeProbability > AI_THRESHOLD) { //definitely AI
-
-          comment.style.border = '1px solid red';
-          comment.innerHTML += `<div style="color: red; font-weight: bold;">Definitely AI - AI probability ${formattedNumberAI}</div>`;
-      }
-       else {
-        comment.style.border = '1px solid yellow';
-        comment.innerHTML += `<div style="color: yellow; font-weight: bold;">Possibly AI - AI probability ${formattedNumberAI}</div>`;
-      }
-     }
+      comment.style.border = '1px solid red';
+      comment.innerHTML += `<div style="color: red; font-weight: bold;">Definitely AI - AI probability ${formattedNumberAI}</div>`;
+    }
+    else {
+      comment.style.border = '1px solid yellow';
+      comment.innerHTML += `<div style="color: yellow; font-weight: bold;">Possibly AI - AI probability ${formattedNumberAI}</div>`;
+    }
+  }
 };
 
+// Run AI detection
 function detectAI(input, callback) {
-  var url = "https://huggingface.co/openai-detector?"+input
+  var detectorUrlWithInput = "https://huggingface.co/openai-detector?" + input
   GM.xmlHttpRequest({
-  method: "GET",
-  url: url,
-  headers: {
-    "User-Agent": "Mozilla/5.0",    // If not specified, navigator.userAgent will be used.
-    "Content-Type": "application/x-www-form-urlencoded"
-    //"Accept": "text/xml"            // If not specified, browser defaults will be used.
-    ,
-  },
+    method: "GET",
+    url: detectorUrlWithInput,
+    headers: {
+      "User-Agent": "Mozilla/5.0",    // If not specified, navigator.userAgent will be used.
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
 
-  onload: function(response) {
-    var responseXML = null;
-    // Inject responseXML into existing Object (only appropriate for XML content).
-    if (!response.responseXML) {
-      responseXML = new DOMParser()
-        .parseFromString(response.responseText, "text/xml");
+    onload: function (response) {
+      var responseParsed = [response.responseText].join("\n");
+      callback(responseParsed);
     }
-    var responseParsed = [response.responseText].join("\n");
-    //console.log("input was: " + input);
-    //addComment(responseParsed);
-    callback(responseParsed);
-  }
-});
+  });
 }
